75219553ebe39b84aac3e36a62de788bb8f5b5c1
Ceddy ST <KidChaor89@Ceddys-MacBook-Air.local>
2018-08-27 21:40:32 -0400

    Added more projects for nth layer dev

---
+++ Website.DataAccessLayer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Website.DataAccessLayer.Global" %>
---
+++ Website.Domain/Default.aspx.designer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Website.Domain
+{
+
+    public partial class Default
+    {
+        protected System.Web.UI.WebControls.Button button1;
+    }
+}
